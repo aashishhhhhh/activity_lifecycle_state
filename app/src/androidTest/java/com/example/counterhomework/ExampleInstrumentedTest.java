2dbbe519dd2c9aee0bd9107f325baa1a30bf3735
aashishhhhhh <47654151+aashishhhhhh@users.noreply.github.com>
2021-03-18 21:08:42 +0545

    CounterHomework

--- app/src/androidTest/java/com/example/counterhomework/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/counterhomework/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.shoppinglist;
+package com.example.counterhomework;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.shoppinglist", appContext.getPackageName());
+        assertEquals("com.example.counterhomework", appContext.getPackageName());
     }
 }
